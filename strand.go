7e8349922779af402fd7cc0b5d6a221eb14a2507
David Zhang <gwwfps@gmail.com>
2015-06-19 00:22:03 -0400

    Reading configuration from env variables and converting upload body to template

--- strand.go
+++ strand.go
@@ -1,67 +1,125 @@
 package main
 
 import (
-	"flag"
+	"bytes"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
+	"text/template"
+
+	"github.com/kelseyhightower/envconfig"
 )
 
-type requestHandler struct {
-	commandToken string
-	apiToken     string
+type RequestHandler struct {
+	Address        string
+	CommandToken   string `envconfig:"command_token"`
+	ApiToken       string `envconfig:"api_token"`
+	TemplateString string `envconfig:"template"`
+	Template       *template.Template
+}
+
+type Thread struct {
+	ChannelName string
+	ChannelId   string
+	UserName    string
+	Title       string
 }
 
 func main() {
-	if len(os.Args) < 3 {
-		log.Fatalln("Usage: strandserver [-address=<address>] <slack command token> <upload API token>")
-	}
+	handler := initSettings()
 
-	address := flag.String("address", "0.0.0.0:8080", "address to listen on")
-	flag.Parse()
-	handler := requestHandler{commandToken: flag.Arg(0), apiToken: flag.Arg(1)}
+	http.Handle("/", handler)
 
-	http.Handle("/", &handler)
+	log.Printf("Listening on %s\n", handler.Address)
 
-	log.Printf("Listening on %s\n", *address)
-
-	log.Fatalln(http.ListenAndServe(*address, nil))
+	log.Fatalln(http.ListenAndServe(handler.Address, nil))
 }
 
-func (handler *requestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func initSettings() *RequestHandler {
+	var handler RequestHandler
+
+	err := envconfig.Process("strand", &handler)
+	if err != nil {
+		log.Fatal("Cannot parse settings:", err)
+	}
+
+	if handler.Address == "" {
+		handler.Address = "0.0.0.0:8080"
+	}
+
+	if handler.CommandToken == "" || handler.ApiToken == "" {
+		log.Fatalln("Command token and API token must be provided through environment variables: STRAND_COMMAND_TOKEN and STRAND_API_TOKEN")
+	}
+
+	if handler.TemplateString == "" {
+		handler.TemplateString = "# {{.Title}}\n\n_This fake thread is auto-generated on behalf of **{{.UserName}}**._"
+	}
+
+	handler.Template, err = template.New("uploadTemplate").Parse(handler.TemplateString)
+	if err != nil {
+		log.Fatal("Cannot parse template:", err)
+	}
+
+	return &handler
+}
+
+func (handler *RequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Println("Incoming request:", r)
 
 	incomingToken := r.PostFormValue("token")
-	if incomingToken != handler.commandToken {
-		log.Println(incomingToken, handler.commandToken)
+	if incomingToken != handler.CommandToken {
+		log.Println(incomingToken, handler.CommandToken)
 		log.Println("Non-matching token in message, ignored.")
 		w.WriteHeader(401)
 		return
 	}
 
 	threadTitle := r.PostFormValue("text")
-	channelName := r.PostFormValue("channel_name")
-	channelId := r.PostFormValue("channel_id")
-	userName := r.PostFormValue("user_name")
 
-	go uploadFile(handler.apiToken, threadTitle, userName, channelId)
+	if threadTitle == "" {
+		log.Println("Empty request, ignoring.")
+		fmt.Fprintf(w, "Please provide a topic.")
+		return
+	}
 
-	fmt.Fprintf(w, "File upload to \"%s\" for topic \"%s\" on its way.", channelName, threadTitle)
+	thread := Thread{
+		Title:       threadTitle,
+		ChannelName: r.PostFormValue("channel_name"),
+		ChannelId:   r.PostFormValue("channel_id"),
+		UserName:    r.PostFormValue("user_name"),
+	}
+
+	err := handler.uploadFile(&thread)
+	if err != nil {
+		fmt.Fprintf(w, "Thread upload failed: %s", err.Error())
+	} else {
+		fmt.Fprintf(w, "File uploaded to \"%s\" for topic \"%s\".\n", thread.ChannelName, thread.Title)
+	}
 }
 
-func uploadFile(apiToken string, threadTitle string, userName string, channelId string) {
+func (handler *RequestHandler) uploadFile(thread *Thread) error {
 	formData := url.Values{}
-	formData.Set("token", apiToken)
-	formData.Set("content", fmt.Sprintf("Thread topic: %s\nThis fake thread is auto-generated on behalf of @%s.", threadTitle, userName))
-	formData.Set("channels", channelId)
-	formData.Set("title", fmt.Sprintf("%s's thread: %s", userName, threadTitle))
+	formData.Set("token", handler.ApiToken)
+	formData.Set("filetype", "post")
+	formData.Set("channels", thread.ChannelId)
+
+	var content bytes.Buffer
+	err := handler.Template.Execute(&content, thread)
+	if err != nil {
+		log.Println("Cannot parse template:", err)
+		return errors.New("There's a problem constructing the file to upload.")
+	}
+	formData.Set("content", content.String())
+
+	formData.Set("title", fmt.Sprintf("Thread: %s", thread.Title))
 
 	resp, err := http.PostForm("https://slack.com/api/files.upload", formData)
 	if err != nil {
-		log.Println("Error uploading file:", err)
+		log.Println("Error uploading file:", err, resp)
+		return errors.New(fmt.Sprintf("Upload request failed with status %s", resp.Status))
 	} else {
 		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
@@ -70,4 +128,6 @@ func uploadFile(apiToken string, threadTitle string, userName string, channelId 
 		}
 		log.Println("File uploaded:", string(body))
 	}
+
+	return nil
 }
