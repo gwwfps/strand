f6b54d580d8df3122b3f9d35284f4c06e0d858cc
David Zhang <gwwfps@gmail.com>
2015-06-22 20:46:02 -0400

    added templating for title

--- strand.go
+++ strand.go
@@ -15,11 +15,13 @@ import (
 )
 
 type RequestHandler struct {
-	Address        string
-	CommandToken   string `envconfig:"command_token"`
-	ApiToken       string `envconfig:"api_token"`
-	TemplateString string `envconfig:"template"`
-	Template       *template.Template
+	Address             string
+	CommandToken        string `envconfig:"command_token"`
+	ApiToken            string `envconfig:"api_token"`
+	BodyTemplateString  string `envconfig:"template"`
+	BodyTemplate        *template.Template
+	TitleTemplateString string `envconfig:"title"`
+	TitleTemplate       *template.Template
 }
 
 type Thread struct {
@@ -56,16 +58,26 @@ func initSettings() *RequestHandler {
 		log.Fatalln("Command token and API token must be provided through environment variables: STRAND_COMMAND_TOKEN and STRAND_API_TOKEN")
 	}
 
-	if handler.TemplateString == "" {
-		handler.TemplateString = "# {{.Title}}\n\n_This fake thread is auto-generated on behalf of **{{.UserName}}**._"
+	if handler.BodyTemplateString == "" {
+		handler.BodyTemplateString = "# {{.Title}}\n\n_This fake thread is auto-generated on behalf of **{{.UserName}}**._"
 	}
 
-	handler.Template, err = template.New("uploadTemplate").Parse(handler.TemplateString)
-	if err != nil {
-		log.Fatal("Cannot parse template:", err)
+	if handler.TitleTemplateString == "" {
+		handler.TitleTemplateString = "{{.Title}}"
 	}
+
+	handler.BodyTemplate = compileTemplate("bodyTemplate", handler.BodyTemplateString)
+	handler.TitleTemplate = compileTemplate("titleTemplate", handler.TitleTemplateString)
 
 	return &handler
+}
+
+func compileTemplate(name string, templateString string) *template.Template {
+	template, err := template.New("titleTemplate").Parse(templateString)
+	if err != nil {
+		log.Fatal("Cannot parse template:", name, err)
+	}
+	return template
 }
 
 func (handler *RequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -108,15 +120,19 @@ func (handler *RequestHandler) uploadFile(thread *Thread) error {
 	formData.Set("filetype", "post")
 	formData.Set("channels", thread.ChannelId)
 
-	var content bytes.Buffer
-	err := handler.Template.Execute(&content, thread)
+	var bodyContent, titleContent bytes.Buffer
+	err := handler.BodyTemplate.Execute(&bodyContent, thread)
+	if err == nil {
+		err = handler.TitleTemplate.Execute(&titleContent, thread)
+	}
+
 	if err != nil {
 		log.Println("Cannot parse template:", err)
 		return errors.New("There's a problem constructing the file to upload.")
 	}
-	formData.Set("content", content.String())
 
-	formData.Set("title", fmt.Sprintf("Thread: %s", thread.Title))
+	formData.Set("content", bodyContent.String())
+	formData.Set("title", titleContent.String())
 
 	resp, err := http.PostForm("https://slack.com/api/files.upload", formData)
 	if err != nil {
